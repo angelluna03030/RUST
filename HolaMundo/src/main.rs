6976880ba8b8e5d36ffcaff05e8fe59ce24e5597
angel luna <angelstivengarcialuna@gmail.com>
2025-03-30 21:50:20 -0500

    Condicionales en RUST

--- HolaMundo/src/main.rs
+++ HolaMundo/src/main.rs
@@ -1,30 +1,11 @@
+
 fn main() {
- let suma2 = suma(3,4);
- println!("La suma es: {}", suma2);
-saludo("Juan Carlos");
-println!("El número es: {}", trece());
+let numero = true;
+if !numero {
+println!("El número no es 3");
+}else if numero{
+    println!("El número es 3");
 }
 
 
-
-fn suma (x: i32, y: i32) -> i32 {
-    println!("Hola desde la función mifuncion");
-    return  x + y;
-}
-
-/// Prints a greeting message with the given name.
-///
-/// # Arguments
-///
-/// * `name` - A string slice that holds the name to be greeted.
-///
-/// # Examples
-///
-/// ```
-/// saludo("Alice");
-/// ```
-fn saludo (name: &str) {
-    println!("Hola mi nombre es: {}", name);
-}fn trece ()->i8{
-    return  13;
 }
