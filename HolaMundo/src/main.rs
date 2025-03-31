4308dd312741c69925ea1d9a5f14911bc23ff9c4
angel luna <angelstivengarcialuna@gmail.com>
2025-03-30 21:44:12 -0500

    Funciones en RUST

--- HolaMundo/src/main.rs
+++ HolaMundo/src/main.rs
@@ -1,15 +1,30 @@
 fn main() {
-    let suma:i32 = 58 + 57;
-    let resta    = 34-35;
-    let multiplicacion   = 23 * 2;
-    let division     = 45 / 5;
-    let modulo:i32   = 45 % 5;
-    let falso:bool = false;
-    let verdadero:bool = true;
-    let emogis = '😎';
-    let tupla: (i32, i32, &str, f64) =(34,3, "fhlo,g", -33.3);
-    let (_a, b, c,_dd): (i32, i32, &str, f64) = tupla;
-    println!("el sungundo varol de tupla es de {}", b);
-    let matrix= [34,34,34,3,53,];
+ let suma2 = suma(3,4);
+ println!("La suma es: {}", suma2);
+saludo("Juan Carlos");
+println!("El número es: {}", trece());
+}
 
+
+
+fn suma (x: i32, y: i32) -> i32 {
+    println!("Hola desde la función mifuncion");
+    return  x + y;
+}
+
+/// Prints a greeting message with the given name.
+///
+/// # Arguments
+///
+/// * `name` - A string slice that holds the name to be greeted.
+///
+/// # Examples
+///
+/// ```
+/// saludo("Alice");
+/// ```
+fn saludo (name: &str) {
+    println!("Hola mi nombre es: {}", name);
+}fn trece ()->i8{
+    return  13;
 }
