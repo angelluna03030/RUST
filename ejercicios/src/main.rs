5ccb4f28b874b7af1bb97a6b9ac1238063317b01
angel luna <angelstivengarcialuna@gmail.com>
2025-04-05 22:24:40 -0500

    solucion de problemas

--- ejercicios/src/main.rs
+++ ejercicios/src/main.rs
@@ -1,26 +1,26 @@
 
-//Arear de un rectangulo 
-struct  Area(i32, i32);
+// //Arear de un rectangulo 
+// struct  Area(i32, i32);
 fn main() {
-   let ancho1 = 15;
-    let alto1 = 10;
-    let area1 = area(ancho1, alto1);
-    println!("El area del rectangulo es: {}", area1);
-    let ancho2 = 15 ;
-    let alto2 = 15 ;
+//    let ancho1 = 15;
+//     let alto1 = 10;
+//     let area1 = area(ancho1, alto1);
+//     println!("El area del rectangulo es: {}", area1);
+//     let ancho2 = 15 ;
+//     let alto2 = 15 ;
 
 
-println!("El area del rectangulo es: {}", areacontuplas(Area(ancho2, alto2)));
-    //Solucion dos 
+// println!("El area del rectangulo es: {}", areacontuplas(Area(ancho2, alto2)));
+//     //Solucion dos 
 }
 
 
-fn area(ancho: u32, alto: u32) -> u32 {
-    let area = ancho * alto;
-    return area;
-}
+// fn area(ancho: u32, alto: u32) -> u32 {
+//     let area = ancho * alto;
+//     return area;
+// }
 
-fn areacontuplas(area:Area) -> i32 {
-    let Area(ancho, alto) = area;
-    return ancho * alto;
-}
+// fn areacontuplas(area:Area) -> i32 {
+//     let Area(ancho, alto) = area;
+//     return ancho * alto;
+// }
